364db4cd121d6106e479043814eb035c04850743
julianbiblocq <julianbiblocq@users.noreply.github.com>
2026-06-11 15:23:09 +0200

    Updates

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-const CACHE_NAME = 'baquemix-cache-v13';
+const CACHE_NAME = 'baquemix-cache-v21';
 
 // Core static files to cache immediately on SW install
 const STATIC_ASSETS = [
