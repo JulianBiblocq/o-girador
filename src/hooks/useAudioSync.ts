ef11e3d2ee3e854d9d5cba37b4646aafa4a8d4c4
julianbiblocq <julianbiblocq@users.noreply.github.com>
2026-07-06 00:52:41 +0200

    refactor: extract native synths from useAudioSync

--- src/hooks/useAudioSync.ts
+++ src/hooks/useAudioSync.ts
@@ -54,94 +54,18 @@ export const voiceSynths: { [id: string]: any } = {};
 export let audioEngine: AudioEngine | null = null;
 export let inputManager: InputManager | null = null;
 
-export const activeNativeOscillators = new Set<OscillatorNode>();
+import {
+  activeNativeOscillators,
+  playNativeMetroClick,
+  playNativeVoiceSynth,
+  stopAllNativeOscillators
+} from '../audio/nativeSynths';
 
-export const playNativeMetroClick = (time: number, isAccent: boolean, soundType: string, volume: number) => {
-  const rawCtx = Tone.getContext().rawContext as AudioContext;
-  const osc = rawCtx.createOscillator();
-  const clickGain = rawCtx.createGain();
-  osc.connect(clickGain);
-  
-  if (metroChannel) {
-    Tone.connect(clickGain, metroChannel as any);
-  } else if (masterVolumeNode) {
-    Tone.connect(clickGain, masterVolumeNode as any);
-  } else {
-    clickGain.connect(rawCtx.destination);
-  }
-
-  const freq = isAccent ? 880 : 440;
-  const volumeMultiplier = isAccent ? 1.0 : 0.6;
-  const finalVol = volume * volumeMultiplier;
-
-  clickGain.gain.setValueAtTime(0.0001, time);
-  clickGain.gain.exponentialRampToValueAtTime(finalVol, time + 0.002);
-
-  if (soundType === 'clave') {
-    osc.type = 'sine';
-    const targetFreq = isAccent ? 1200 : 800;
-    osc.frequency.setValueAtTime(targetFreq * 2, time);
-    osc.frequency.exponentialRampToValueAtTime(targetFreq, time + 0.01);
-    clickGain.gain.exponentialRampToValueAtTime(0.0001, time + 0.08);
-    osc.start(time);
-    osc.stop(time + 0.09);
-  } else if (soundType === 'cowbell') {
-    osc.type = 'triangle';
-    osc.frequency.value = isAccent ? 587.33 : 440;
-    clickGain.gain.exponentialRampToValueAtTime(0.0001, time + 0.06);
-    osc.start(time);
-    osc.stop(time + 0.07);
-  } else {
-    osc.type = 'square';
-    osc.frequency.value = freq;
-    clickGain.gain.exponentialRampToValueAtTime(0.0001, time + 0.04);
-    osc.start(time);
-    osc.stop(time + 0.05);
-  }
-
-  activeNativeOscillators.add(osc);
-  osc.onended = () => {
-    activeNativeOscillators.delete(osc);
-    try {
-      osc.disconnect();
-      clickGain.disconnect();
-    } catch (_) {}
-  };
-};
-
-export const playNativeVoiceSynth = (freq: number, time: number, duration: number, volume: number, channelNode: any) => {
-  const rawCtx = Tone.getContext().rawContext as AudioContext;
-  const osc = rawCtx.createOscillator();
-  const voiceGain = rawCtx.createGain();
-  osc.connect(voiceGain);
-
-  if (channelNode) {
-    Tone.connect(voiceGain, channelNode);
-  } else if (masterVolumeNode) {
-    Tone.connect(voiceGain, masterVolumeNode as any);
-  } else {
-    voiceGain.connect(rawCtx.destination);
-  }
-
-  osc.type = 'triangle';
-  osc.frequency.setValueAtTime(freq, time);
-
-  const attack = 0.05;
-  voiceGain.gain.setValueAtTime(0.0001, time);
-  voiceGain.gain.exponentialRampToValueAtTime(volume * 0.3, time + attack);
-  voiceGain.gain.exponentialRampToValueAtTime(0.0001, time + duration);
-
-  osc.start(time);
-  osc.stop(time + duration);
-
-  activeNativeOscillators.add(osc);
-  osc.onended = () => {
-    activeNativeOscillators.delete(osc);
-    try {
-      osc.disconnect();
-      voiceGain.disconnect();
-    } catch (_) {}
-  };
+export {
+  activeNativeOscillators,
+  playNativeMetroClick,
+  playNativeVoiceSynth,
+  stopAllNativeOscillators
 };
 
 // Performance caches and pools
@@ -1135,10 +1059,7 @@ export function useAudioSync({
         audioEngine = null;
       }
       inputManager = null;
-      activeNativeOscillators.forEach((osc) => {
-        try { osc.stop(); osc.disconnect(); } catch (_) {}
-      });
-      activeNativeOscillators.clear();
+      stopAllNativeOscillators();
       isAudioInitializedRef.current = false;
     };
   }, []);
@@ -1205,10 +1126,7 @@ export function useAudioSync({
         stopVocalRecording();
       }
       audioEngine?.stopAllBarulho();
-      activeNativeOscillators.forEach((osc) => {
-        try { osc.stop(); osc.disconnect(); } catch (_) {}
-      });
-      activeNativeOscillators.clear();
+      stopAllNativeOscillators();
 
       (Object.values(vocalPlayersRef.current) as any[]).forEach((player) => {
         try {
@@ -1252,10 +1170,7 @@ export function useAudioSync({
       stopVocalRecording();
     }
     audioEngine?.stopAllBarulho();
-    activeNativeOscillators.forEach((osc) => {
-      try { osc.stop(); osc.disconnect(); } catch (_) {}
-    });
-    activeNativeOscillators.clear();
+    stopAllNativeOscillators();
 
     (Object.values(vocalPlayersRef.current) as any[]).forEach((player) => {
       try {
@@ -1295,10 +1210,7 @@ export function useAudioSync({
     audioEngine?.stop();
     Tone.Transport.stop();
     audioEngine?.stopAllBarulho();
-    activeNativeOscillators.forEach((osc) => {
-      try { osc.stop(); osc.disconnect(); } catch (_) {}
-    });
-    activeNativeOscillators.clear();
+    stopAllNativeOscillators();
 
     (Object.values(vocalPlayersRef.current) as any[]).forEach((player) => {
       try {
